51b8444e08f1adf559bd4664bbbd5d19aeb99383
Carlos Heitor Lain <atendimento@virtuati.com.br>
2013-01-03 17:14:03 -0200

    Algumas mudanças

---
+++ src/config/versao.h
@@ -0,0 +1,14 @@
+// Copyright (c) Time de Desenvolvimento Cronus-Emulator - Licensed under GNU GPL
+// For more information, see LICENCE in the main folder
+#ifndef _CONFIG_VERSAO_H_
+#define _CONFIG_VERSAO_H_
+
+//Se habilitado (descomentado), a Renovação estará ativa
+//#define RENEWAL
+
+//Se habilitado (descomentado), a versão Old Times estará ativa 
+//#define OLD_TIMES
+
+//Se as duas definições estiverem desabilitadas, a Pré-Renovação estará ativa
+
+#endif // _CONFIG_VERSAO_H_
